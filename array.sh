1016f7fe62a22a3f082f72d4e9c9fb7ea8cb2f23
parameswara98 <parameswarareddy98@gmail.com>
2025-05-15 18:40:35 +0100

    hello world

--- array.sh
+++ array.sh
@@ -2,6 +2,6 @@
 
 movies={"court","money","parameswara"}
 
-echo " first movie is ${movies[0]}
+echo " first movie is ${movies[0]}"
 
-echo "first movie is ${movies[2]}
+echo "first movie is ${movies[2]}"
